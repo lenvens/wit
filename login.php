7f5203020107b45f0bb36a72414c9fdfeb440911
lenvens <lenvens@163.com>
2013-06-21 14:08:34 +0800

    login&register

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+use witclass\User_t;
+
+
+require_once 'doctrine.php';
+$login=$_POST['login'];
+$pwd=$_POST['pwd'];
+$sbt=$_POST['sbt'];
+if(isset($sbt))
+{
+
+
+	$eee = $em->createQuery('SELECT u FROM po\MyPoA u where u.name=\''.$login.'\' and u.password=\''.$pwd.'\'');
+	$users = $eee->getArrayResult();
+	if($users)
+	{
+		$_SESSION['wt']='wt';
+		Header("Location:home.php");
+
+	
+	}
+	else
+	{
+		echo "dfdfdfd";
+	
+	}
+	//$a=new MyPoA;
+	//$a->setMyPoAName('weitao');
+
+	//$b=new MyPoB;
+	//$b->setMyPoBNick('weitao');
+	//$b->setMyPoAbid($a);
+	//$em->persist($a);
+	//$em->persist($b);
+	//$em->flush();
+}
+else
+{
+?>
+<form method="post" action="<?=$PHP_SELF?>">
+<p>name:<input type="text" name="login" /></p>
+<p>pawd:<input type="password" name="pwd" /></p>
+<p><input name="sbt" type="submit" value="submit" /></p>
+</form>
+<?
+}
+?>
