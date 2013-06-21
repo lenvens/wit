11c4f3e4a0a04e8d8de3533dad098de49bd74e6f
lenvens <lenvens@163.com>
2013-06-21 15:30:08 +0800

    bootstrap login&register

--- login.php
+++ login.php
@@ -42,7 +42,7 @@ else
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Sign in &middot; Twitter Bootstrap</title>
+    <title>Sign in </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
